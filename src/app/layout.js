920932e76d3038d6888869138a3c64b0cfe086c9
yuta asakura <yuta.asakura.se@gmail.com>
2024-09-06 14:56:30 +0900

    added resources

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,23 @@
-import localFont from "next/font/local";
+import localFont from "@next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const poppins = localFont({
+  src: [
+  {
+  path: "../../public/fonts/Recoleta-Black.ttf",
+  weight: "400",
+  },
+  {
+  path: "../../public/fonts/Recoleta-Medium.ttf",
+  weight: "300",
+  },
+  {
+  path: "../../public/fonts/Recoleta-Light.ttf",
+  weight: "200",
+  },
+  ],
+  variable: "--font-Recoleta-Black",
+  });
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +28,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={poppins.className}
       >
         {children}
       </body>
