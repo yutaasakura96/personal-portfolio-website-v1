3f3225179ee8494f7462e1d8255f8ea5a0ec93c2
yuta asakura <yuta.asakura.se@gmail.com>
2024-09-08 20:42:23 +0900

    added interactive card section with hover effects, animations, and dynamic selection for about me skills section

--- src/app/components/AboutMe/AboutMe.js
+++ src/app/components/AboutMe/AboutMe.js
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Hind } from "next/font/google";
+
+import { AboutData } from "@/app/data";
 
 const hind = Hind({
   subsets: ["latin"],
@@ -7,6 +10,25 @@ const hind = Hind({
 });
 
 export default function AboutMe() {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isFaded, setIsFaded] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [mapData, setMapData] = useState(AboutData[0]);
+
+  const myFunctions = (data) => {
+    setIsFlipped(false);
+    setIsFaded(false);
+    setMapData(data);
+  };
+
+  const handleCardClick = (data, index) => {
+    setIsFlipped(true);
+    setIsFaded(true);
+    setSelectedIndex(index);
+
+    setTimeout(() => myFunctions(data), 600);
+  };
+
   return (
     <React.Fragment>
       <div
@@ -57,6 +79,59 @@ export default function AboutMe() {
             </p>
           </section>
         </div>
+      </div>
+
+      <div
+        style={{
+          backgroundImage: "linear-gradient(45deg, #EAF7FC  70%,#48AFDE 30%)",
+          width: "100%",
+        }}
+        className="lg:-mt-60"
+      >
+        <section className="container flex  flex-col m-auto sm:flex-row px-5 md:px-24 mt-[50px sm:mt-0] transform translate-y-[-100px]">
+          <div className=" hidden sm:flex w-full sm:w-1/2 lg:w-7/12">
+            <div className="grid sm:grid-cols-2 xl:grid-cols-2 gap-6 mr-0 lg:mr-10">
+              {AboutData.map((item, index) => (
+                <a
+                  key={index}
+                  onClick={() => handleCardClick(item, index)}
+                  style={{ boxShadow: "#48AFDE -5px  10px 20px 0px" }}
+                  className={`relative cursor-pointer transition-all transform duration-300 group rounded-xl center p-6 lg:p-10 flex flex-col justify-center  items-center ${
+                    selectedIndex == index
+                      ? " -translate-y-2 bg-[#476571]"
+                      : "hover:bg-[#476571] hover:shadow-xl hover:-translate-y-2 bg-white"
+                  }`}
+                >
+                  <div className="w-16 h-16 sm:w-10 sm:h-10  lg:w-16 lg:h-16">
+                    <img
+                      height={100}
+                      width={100}
+                      src={item.img}
+                      alt="internet issues"
+                    />
+                  </div>
+                  <h4
+                    className={`text-center text-sm lg:text-xl  font-recoletaBold transition-colors duration-500 group-hover:text-white text-[#47626D] mt-3 ${
+                      selectedIndex === index ? "text-white" : ""
+                    }`}
+                  >
+                    {item.title}
+                  </h4>
+
+                  <div
+                    className={`absolute -top-2 -right-2 transform transition-all duration-500 opacity-0 group-hover:opacity-100 shadow-xl w-12 h-12 rounded-lg bg-[#476571] flex justify-center items-center font-bold text-white font-recoletaBold text-xl  ${
+                      selectedIndex === index
+                        ? "opacity-100 rotate-12"
+                        : "group-hover:rotate-12"
+                    }`}
+                  >
+                    {item.count}
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
+        </section>
       </div>
     </React.Fragment>
   );
