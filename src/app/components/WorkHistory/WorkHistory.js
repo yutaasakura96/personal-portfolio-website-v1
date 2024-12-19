10ec9bb0a024ab244bec37f736b8a55f9b2d07af
yuta asakura <yuta.asakura.se@gmail.com>
2024-12-19 22:33:26 +0900

    feat: improve SEO and accessibility in WorkHistory component
    Enhanced the WorkHistory component for better SEO and accessibility:
    - Replaced <div> with <section> and <ul> with <article> for semantic HTML structure.
    - Added ARIA attributes such as 'aria-labelledby' and 'aria-label' for improved screen reader support.
    - Updated headings to maintain a proper hierarchy ('<h1>' for title, '<h2>' and '<h3>' for subsections).
    - Refined alt text for images to improve relevance and keyword optimization.
    - Adjusted text styles and structure for better readability and search engine indexing.

--- src/app/components/WorkHistory/WorkHistory.js
+++ src/app/components/WorkHistory/WorkHistory.js
@@ -15,59 +15,69 @@ export default function WorkHistory() {
   const workExperiences = t.raw("experiences");
 
   return (
-    <div
+    <section
       id="work-history-component"
       className="bg-[##eef7fb] lg:h-100 pt-24 pb-24"
       style={{
         backgroundImage: "linear-gradient(90deg, #eef7fb 0 50%, #E0F3FD 0% 100%)",
         width: "100%",
       }}
+      aria-labelledby="work-history-title"
     >
       <div className="container m-auto">
-        <h1 className="text-5xl text-[#48AFDE] mb-10 md:px-24 px-5">
+        <h1 id="work-history-title" className="text-5xl text-[#48AFDE] mb-10 md:px-24 px-5">
           {t("title")}
         </h1>
-        <ul className="space-y-6">
+        <div>
           {workExperiences.map((experience, index) => (
-            <li
+            <article
               key={index}
               style={{ boxShadow: "#48AFDE -5px 10px 20px 0px" }}
               className="p-6 lg:p-10 rounded-xl bg-white mb-10 transition-all transform duration-300 hover:shadow-xl hover:-translate-y-2 cursor-pointer"
+              aria-labelledby={`work-experience-${index}-title`}
             >
               <a
                 href={experience.website}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="block"
+                aria-label={`Learn more about ${experience.company}`}
               >
                 <div className="flex flex-col mb-4">
-                    <Image
-                      src={experience.logo}
-                      alt={`${experience.company} logo`}
-                      width={112}
-                      height={112}
-                      className="w-28 h-28 mr-4 mb-5"
-                    />
-                  <h4 className="text-3xl font-semibold text-[#48AFDE]">
+                  <Image
+                    src={experience.logo}
+                    alt={`${experience.company} logo`}
+                    width={112}
+                    height={112}
+                    className="w-28 h-28 mr-4 mb-5"
+                  />
+                  <h2
+                    id={`work-experience-${index}-title`}
+                    className="text-3xl font-semibold text-[#48AFDE]"
+                  >
                     {experience.company}
-                  </h4>
+                  </h2>
                   {experience.position ? (
                     <>
-                      <p className="text-lg font-normal">
-                        {experience.position}
-                      </p>
+                      <p className="text-lg font-normal">{experience.position}</p>
                       <p className="text-sm font-normal text-[#223740]">
                         {experience.start} - {experience.end}
                       </p>
                       <p className="text-sm font-normal text-[#223740]">
                         {experience.location}
                       </p>
-                      <p className={`mt-2 text-[#223740] mr-0 mb-5 lg:mr-4 font-[500] ${hind.className} text-[16px] leading-7`}>
+                      <p
+                        className={`mt-2 text-[#223740] mr-0 mb-5 lg:mr-4 font-[500] ${hind.className} text-[16px] leading-7`}
+                      >
                         {experience.description.join(" ")}
                       </p>
-                      <h4><strong>{experience.techStackTitle}:</strong></h4>
+                      <h3>
+                        <strong>{experience.techStackTitle}:</strong>
+                      </h3>
                       {experience.techStack && experience.techStack.length > 0 && (
-                        <p className={`mt-2 text-[#223740] mr-0 mb-5 lg:mr-4 font-[500] ${hind.className} text-[16px] leading-7`}>
+                        <p
+                          className={`mt-2 text-[#223740] mr-0 mb-5 lg:mr-4 font-[500] ${hind.className} text-[16px] leading-7`}
+                        >
                           {experience.techStack.join(", ")}
                         </p>
                       )}
@@ -75,11 +85,13 @@ export default function WorkHistory() {
                   ) : (
                     experience.positions.map((position, posIndex) => (
                       <div key={posIndex} className="mt-4">
-                        <p className="text-lg font-normal">{position.title}</p>
+                        <h3 className="text-lg font-normal">{position.title}</h3>
                         <p className="text-sm font-normal text-[#223740]">
                           {position.start} - {position.end}
                         </p>
-                        <p className={`mt-2 text-[#223740] mr-0 mb-5 lg:mr-4 font-[500] ${hind.className} text-[16px] leading-7`}>
+                        <p
+                          className={`mt-2 text-[#223740] mr-0 mb-5 lg:mr-4 font-[500] ${hind.className} text-[16px] leading-7`}
+                        >
                           {position.description.join(" ")}
                         </p>
                         {position.techStack && position.techStack.length > 0 && (
@@ -93,10 +105,10 @@ export default function WorkHistory() {
                   )}
                 </div>
               </a>
-            </li>
+            </article>
           ))}
-        </ul>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
